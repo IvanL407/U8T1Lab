fa1b5e76296db22f34a1e27373fb77377ffa079a
IvanL407 <ivanl58@nycstudents.net>
2022-03-02 19:58:58 -0500

    finished lab

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -2,9 +2,26 @@ public class FunWith2DArrays {
 
     public static int totalElements(int[][] temp)
     {
-        for (int[] num : temp)
+        int numRows = 0;
+        int count = 0;
+        for (int i = 0; i < temp.length; i++)
         {
-
+            numRows = temp[i].length;
+            count++;
         }
+        return (numRows * count);
+    }
+
+    public static void fourCorners(String[][] temp)
+    {
+        int numRows = temp.length;
+        int numColumns = temp[0].length;
+
+
+        System.out.println(temp[0][0]);
+        System.out.println(temp[0][numColumns - 1]);
+        System.out.println(temp[numRows - 1][0]);
+        System.out.println(temp[numRows - 1][numColumns - 1]);
+
     }
 }
